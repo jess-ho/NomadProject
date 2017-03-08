168687d72d7114344863cac3c4a845b5113598f9
Jessica H <Jessica H>
2017-03-08 14:45:16 -0500

    Linked inputs with a value, added a submit button, created form tags

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -15,7 +15,6 @@ nomadApp.getCity = function(city) {
 			key: nomadApp.key
 		}
 	}).then(function(cities) {
-
 		// var nomadCost = cities.result[0].cost.nomad.USD;
 		// var expatCost =cities.result[0].cost.expat.USD;
 		// console.log(accomdationCost, coffeeCost, beerCost, hotelCost);
@@ -32,6 +31,8 @@ nomadApp.getCost = function(cities) {
 	var hotelPerDay = hotelCost / 30;
 	var coffeeCost = cities.result[0].cost.coffee_in_cafe.USD;
 	var beerCost = cities.result[0].cost.beer_in_cafe.USD;
+
+
 	
 }
 
@@ -40,9 +41,20 @@ nomadApp.events = function() {
 	$('form').on('submit', function(e) {
 		e.preventDefault();
 
-		// retrieve the user's inputs
+	// retrieve the user's inputs
 		
+		// total budget 
+		var budget = $('#budget').val();
+		// airbnb or hotel
+		var stay = $('[name=accommodation]:checked').val();
+		// how many pints/day
+		var alcohol = $('[name=alcohol]:checked').val();
+		// how many cups/day
+		var coffee = $('[name=coffee]:checked').val();	
 
+		console.log(`${budget} ${stay} ${alcohol} ${coffee}`)
+
+		// nomadApp.getCity(cities);
 	});
 }
 
@@ -52,7 +64,8 @@ nomadApp.events = function() {
 // we take out country and state, user only sees city name
 
 nomadApp.init = function () {
-	nomadApp.getCity('amsterdam-netherlands')
+	// nomadApp.getCity('amsterdam-netherlands')
+	nomadApp.events()
 }
 
 $(function() {
