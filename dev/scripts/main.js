fdf77afde2bf1ac20fe980b4e349088e30ed3af8
JessicaH <jessica@lootmarket.com>
2019-12-05 22:42:01 -0500

    Fix API errors

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -34,47 +34,31 @@ nomadApp.getCityInfo = function(cityName) {
 			client: nomadApp.client,
 			key: nomadApp.key,
 		}
-	}).then(function(cityOptions) {
+	}).then(function(options) {
 		// variables for each cost needed, makes sure there are cost results first
-		if (cityOptions.result[0] !== undefined) {
-			var airbnbCost = cityOptions.result[0].cost.airbnb_median.USD;
-		} 
-		if (cityOptions.result[0] !== undefined) {
-			var hotelCost = cityOptions.result[0].cost.hotel.USD;
-		} 
-		if (cityOptions.result[0] !== undefined) {
-			var coffeeCost = cityOptions.result[0].cost.coffee_in_cafe.USD;
-		} 
-		if (cityOptions.result[0] !== undefined) {
-			var beerCost = cityOptions.result[0].cost.beer_in_cafe.USD;
-		} 
-		if (cityOptions.result[0] !== undefined) {
-			var nomadCost = cityOptions.result[0].cost.nomad.USD;
-		} 
-		if (cityOptions.result[0] !== undefined) {
-			var wifiSpeed = cityOptions.result[0].info.internet.speed.download;
+		var cityOptions = options.result[0];
+		if (cityOptions === undefined) {
+			return;
 		}
-		if (cityOptions.result[0] !== undefined) {
-			var weatherAverage = cityOptions.result[0].info.weather.temperature.celsius;
-		}
-		if (cityOptions.result[0] !== undefined) {
-			var cityImageName = cityOptions.result[0].info.city.name;
-		}
+		var airbnbCost = cityOptions.cost.shortTerm.USD;
+		var hotelCost = cityOptions.cost.longTerm.USD;
+		var wifiSpeed = cityOptions.info.internet.speed.download;
+		var weatherAverage = cityOptions.info.weather.temperature.celsius;
+		var cityImageName = cityOptions.info.city.name;
 		// url for image
-		if (cityOptions.result[0] !== undefined) {
-			var cityImage1500 = `https://nomadlist.com${cityOptions.result[0].media.image["1000"]}`;
-		} 
-		// total budget 
+		var imagePath = `/assets/img/cities/${cityOptions.info.city.slug}-1000px.jpg`;
+		var cityImage1500 = `https://nomadlist.com${imagePath}`;
+		// total budget
 		var budget = $('#budget').val();
 		// airbnb or hotel
 		var stay = $('.housing.active').data('value');
 		// how many pints/day
-		var alcohol = $('.beer.active').data('value');
+		// var alcohol = $('.beer.active').data('value');
 		// how many cups/day
-		var coffee = $('.cafe.active').data('value');
-		// calculating individual costs per day 
-		var alcoholPerDay = Math.round((alcohol * beerCost) * 100) / 100;
-		var coffeePerDay = Math.round((coffee * coffeeCost) * 100) / 100;
+		// var coffee = $('.cafe.active').data('value');
+		// calculating individual costs per day
+		// var alcoholPerDay = Math.round((alcohol * beerCost) * 100) / 100;
+		// var coffeePerDay = Math.round((coffee * coffeeCost) * 100) / 100;
 		// radio button for hotel or airbnb
 		if (stay === 'airbnb_median') {
 			var stayCost =  airbnbCost;
@@ -82,21 +66,21 @@ nomadApp.getCityInfo = function(cityName) {
 			var stayCost =  hotelCost;
 		}
 		// calculating total costs per day
-		var totalDays = Math.floor(budget / (stayCost + alcoholPerDay + coffeePerDay));
+		var totalDays = Math.floor(budget / stayCost);
 		var cityCleanName = nomadApp.cityName;
-		// if initial costs from api are undefined... 
-		if (airbnbCost === undefined && beerCost === undefined && coffeeCost === undefined && hotelCost === undefined) {
+		// if initial costs from api are undefined...
+		if (airbnbCost === undefined && hotelCost === undefined) {
 			$('.results').html(`
 				<p><span class="capitalize highlight">${cityCleanName.replace(/-/g, " ")}'s</span> information is currently unavailable.</p>
 				`);
 			$('.cityDetails').hide();
 		}
-		// if someone enters negative budget 
+		// if someone enters negative budget
 		else if (budget < 0) {
 			$('.results').html(`
 				<p>Maybe you should get a credit loan first</p>
 				`);
-		} 
+		}
 		// the cost of travel per day in selected city
 		else {
 			$('.results').html(`
@@ -106,8 +90,8 @@ nomadApp.getCityInfo = function(cityName) {
 				$(this).hide();
 				$('.cityImage p').css('position', 'relative');
 			}).attr('src', cityImage1500).attr('alt', `Picture of ${cityCleanName.replace(/-/g, " ")}.`);
-			$('#coffeeCost').text(`$${coffeePerDay} /day`);
-			$('#alcoholCost').text(`$${alcoholPerDay} /day`);
+			// $('#coffeeCost').text(`$${coffeePerDay} /day`);
+			// $('#alcoholCost').text(`$${alcoholPerDay} /day`);
 			$('#stayCost').text(`$${stayCost} /night`);
 			if (wifiSpeed !== 0) {
 				$('#wifiSpeed').text(`${wifiSpeed} /mbps`);
@@ -135,7 +119,7 @@ nomadApp.events = function() {
 	})
 		$('.headerBack').hover(function(){
 			$(this).toggleClass('is-active');
-		})		
+		})
 	$('.housing').click(function() {
 		$('.housing').removeClass('active');
 		$(this).addClass('active');
@@ -166,10 +150,10 @@ nomadApp.events = function() {
 		})
 	$('.resetButton').on('click', function() {
 		location.reload();
-	})	
-		$('.resetButton').hover(function(){
-			$(this).toggleClass('is-active');
-		})	
+	})
+	$('.resetButton').hover(function(){
+		$(this).toggleClass('is-active');
+	})
 	$('#budget').on('change', function() {
 		if ($('#budget').val() !== '') {
 			$('.submitButton').removeAttr('disabled');
